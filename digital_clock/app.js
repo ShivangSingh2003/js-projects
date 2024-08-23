0f36cbfc4c081b50db731002d5315a8c97454c87
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-23 13:13:48 +0000

    wrote the logic for the clock

---
+++ digital_clock/app.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector('#clock')
+
+let date = new Date();
+console.log(date.toLocaleTimeString());
+
+setInterval( function(){
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString()
+    // console.log(date.toLocaleTimeString());
+}, 1000)
